08d66e0a717f34afa523db5bbe3f0192b99f50aa
Kauan Carvalho <kauan.calasans@youse.com.br>
2020-06-26 11:01:54 -0300

    Upgrade seed

--- db/seed/vacancies_seed.rb
+++ db/seed/vacancies_seed.rb
@@ -3,6 +3,15 @@
 puts 'Creating vacancies'
 
 residence = StudentResidence.find_by(name: 'Rep Vegas')
-vacancy = residence.vacancies.create!(gender: :both, description: 'Melhor república da região.', monthly_cost: 507.32, availability: :soon, availability_description: 'Livre à partir de setembro', type: :single_room, square_meter: 25.2)
+vacancy = residence.vacancies.create!(
+  gender: :both,
+  description: 'Melhor república da região.',
+  monthly_cost: 507.32,
+  availability: :soon,
+  availability_description: 'Livre à partir de setembro',
+  type: :single_room,
+  square_meter: 25.2,
+  image: image_file('treehouse.jpg')
+)
 vacancy.commodities << Commodity.find_by(name: 'Suíte')
 vacancy.commodities << Commodity.find_by(name: 'Air Conditioning')
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,14 @@
 Commodity.destroy_all
 StudentResidence.destroy_all
 
+def image_file(file_name)
+  File.open(File.join(Rails.root, 'db', 'seed_images', file_name))
+end
+
 require_relative 'seed/student_residences_seed'
 require_relative 'seed/commodities_seed'
 require_relative 'seed/vacancies_seed'
-require_relative 'seeds/pictures'
+require_relative 'seed/pictures'
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).

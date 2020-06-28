1bb23fe7ad575260f8f060a0697551a39c857af4
Artur <artur.prado@gmail.com>
2020-06-28 11:04:42 -0300

    Include class name on path

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -6,4 +6,8 @@ class PictureUploader < CarrierWave::Uploader::Base
   version :standard do
     process resize_to_fill: [100, 150, :north]
   end
+
+  def public_id
+    return "image-#{Rails.env}/#{model.class.name}/#{model.try(:name) || Time.now.strftime('%Y-%m-%d_%H-%M-%S')}"
+  end
 end

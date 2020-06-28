383ec1eedaa1ab6560678b2ef951f0e7971534da
Kauan Carvalho <kauan.calasans@youse.com.br>
2020-06-28 00:46:31 -0300

    Change main seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,60 @@
-Commodity.destroy_all
-StudentResidence.destroy_all
-Characteristic.destroy_all
-Vacancy.destroy_all
+# frozen_string_literal: true
+
+require 'yaml'
+
+# Useful methods.
 
 def image_file(file_name)
-  File.open(File.join(Rails.root, 'db', 'seed_images', file_name))
+  File.open(File.join(Rails.root, 'db', 'seed', 'pictures', file_name))
 end
 
+def pick_element(file_name)
+  path = Rails.root.join('db', 'seed', 'fixtures', file_name + '.yml')
+  YAML.load(File.read(path))
+end
+
+def red(string)
+  puts "\033[31m#{string}\033[0m"
+end
+
+def green(string)
+  puts "\033[0;32m#{string}\033[0m"
+end
+
+def yellow(string)
+  puts "\033[0;33m#{string}\033[0m"
+end
+
+def pink(string)
+  puts "\033[0;35m#{string}\033[0m"
+end
+
+# Cleaning old records.
+
+puts "\n== Cleaning old records ==", ""
+
+MODELS = [
+  Vacancy,
+  Commodity,
+  StudentResidence,
+  Characteristic,
+  ImageExample
+].freeze
+
+MODELS.each do |model|
+  begin
+    model.destroy_all
+
+    green "Clean records of #{model} -> Success."
+  rescue
+    red "Clean records of #{model} -> Error."
+  end
+end
+
+# Summons individual seeds.
+
 require_relative 'seed/student_residences_seed'
-require_relative 'seed/commodities_seed'
 require_relative 'seed/vacancies_seed'
+require_relative 'seed/commodities_seed'
 require_relative 'seed/characteristics_seed'
 require_relative 'seed/pictures'
-
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)

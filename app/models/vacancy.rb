8b66122db58a4d3ebafbb1e09a48703ea3c45ae8
João Motta <joao.paulo.fm@hotmail.com>
2020-07-16 16:14:37 -0300

    Update app/models/vacancy.rb
    Co-authored-by: Artur Caliendo Prado <artur.prado@gmail.com>

--- app/models/vacancy.rb
+++ app/models/vacancy.rb
@@ -11,7 +11,7 @@ class Vacancy < ApplicationRecord
 
   belongs_to :student_residence, inverse_of: :vacancies
 
-  validates :monthly_cost, :numericality => { :greater_than => 0 }
+  validates :monthly_cost, numericality: { greater_than: 0 }
   validates :availability, :gender, :type, presence: true
 
   mount_uploader :image, UploaderFactory.uploader
